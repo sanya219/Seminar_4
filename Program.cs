7df6c157ef35ba5758b632cd1f672d94b97cd99d
Alexander Daurskikh <sanya219@gmail.com>
2023-02-05 09:03:15 +0100

    Self work

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1
+/*
+int DigitCount(int number)
+{
+    int count = 0;
+    while(number != 0)
+    {
+        count++;
+        number /= 10;
+    }
+    return count;
+}
+
+Console.Write("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Number of digits is: " + DigitCount(num));
+*/
+// Задача 2
+int Factorial(int number)
+{
+    int result = 1;
+    /* for(int i = 1; i <= number; i++)
+        result *= i; */
+        
+    while(number > 1)
+    {
+        result *= number;
+        number--;
+    }
+
+    return result;
+}
+
+Console.Write("Enter number N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Factorial is: " + Factorial(numN));
